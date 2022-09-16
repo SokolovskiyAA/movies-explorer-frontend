9caf0f404517398b16fc3158748f7fd69bfdf690
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-09-16 10:57:11 +0300

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,7 @@ export const socialLinks = [
   {
     id: 1,
     title: "Github",
-    url: "https://github.com/famovkin",
-    modifier: "medium",
-  },
-  {
-    id: 2,
-    title: "Telegram",
-    url: "https://t.me/famovkin",
+    url: "https://github.com/SokolovskiyAA/",
     modifier: "medium",
   },
 ];
@@ -17,17 +11,17 @@ export const projects = [
   {
     id: 1,
     title: "Социальная сеть с фотографиями",
-    url: "https://famovkin.github.io/react-mesto-api-full/",
+    url: "https://sokolovskiyaa.github.io/mesto/",
   },
   {
     id: 2,
-    title: "Интернет-магазин кроссовок",
-    url: "https://famovkin.github.io/react-sneakers/",
+    title: "Адаптивный сайт",
+    url: "https://github.com/SokolovskiyAA/react-mesto-api-full",
   },
   {
     id: 3,
-    title: "Интернет-магазин пиццерии",
-    url: "https://pet-pizza.herokuapp.com/",
+    title: "Одностраничное приложение",
+    url: "https://sokolovskiyaa.github.io/russian-travel/",
   },
 ];
 
@@ -40,27 +34,7 @@ export const footerLinks = [
   {
     id: 2,
     title: "Github",
-    url: "https://github.com/famovkin",
-  },
-  {
-    id: 3,
-    title: "Telegram",
-    url: "https://t.me/famovkin",
-  },
-];
-
-export const diplomaTextCards = [
-  {
-    id: 1,
-    title: "Дипломный проект включал 5 этапов",
-    description:
-      "Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.",
-  },
-  {
-    id: 2,
-    title: "На выполнение диплома ушло 5 недель",
-    description:
-      "У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.",
+    url: "https://github.com/SokolovskiyAA",
   },
 ];
 
@@ -76,7 +50,6 @@ export const technoList = [
 
 export const DEFAULT_ERROR_MESSAGE = "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз";
 export const SERVER_URL = "https://api.nomoreparties.co/";
-export const UNKNOWN_IMAGE_URL = "https://raw.githubusercontent.com/famovkin/movies-explorer-frontend/level-3/src/images/unknown-image.jpeg";
 export const UNKNOWN_TRAILER_URL = "https://www.youtube.com";
 export const UNKNOWN_CARD_TEXT = "Неизвестно";
 export const NOTIFICATION_DURATION = 2500;
